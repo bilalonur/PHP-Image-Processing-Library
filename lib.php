cf1449140ce003eb44cc9fd9da529dc447b3c1c8
Bilal Onur Eskili <boeskili@yahoo.com>
2021-09-06 20:36:28 +0300

    Horizontal Edge Detection added.

--- lib.php
+++ lib.php
@@ -1,11 +1,13 @@
 <?php
 /*
-  // PHP IMPROCS Library \\
-dev: 		Bilal Onur Eskili
-cont@ct:	onur@bilalonureskili.com
+     PHP IMPROCS Library
+
+dev:      Bilal Onur Eskili
+cont@ct:  onur@bilalonureskili.com
 
 Includes:
 Blur Effect => blurEf($url,$level)  ~ 05.09.2021
+Horizontal Edge Detection =>  horizontalEdge($url) ~ 06.09.2021
 */
 
 function blurEf($url,$level) {
@@ -15,6 +17,16 @@ for ($i = 1; $i <= $level; $i++) {
 $emboss = array(array(0.0625, 0.125, 0.0625), array(0.125, 0.25, 0.125), array(0.0625, 0.125, 0.0625));
 imageconvolution($image, $emboss, 1, 0);
 }
+}
+header('Content-Type: image/png');
+imagepng($image, null, 9);
+}
+
+function horizontalEdge($url) {
+$image = imagecreatefrompng($url);
+for ($i = 1; $i <= 2; $i++) {
+$emboss = array(array(1, 1, 1), array(0, 0, 0), array(-1, -1, -1));
+imageconvolution($image, $emboss, 1, 0);
 }
 header('Content-Type: image/png');
 imagepng($image, null, 9);
